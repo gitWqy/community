47a631510d37165e963794fb541f18adfbbe4e8c
wqy <1124693150@qq.com>
2019-10-13 17:26:14 +0800

    项目一阶段完成，后续会追加 20191013

---
+++ src/main/java/life/majiang/community/CommunityApplication.java
@@ -0,0 +1,18 @@
+package life.majiang.community;
+
+import life.majiang.community.exception.CustomizeErrorCode;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@MapperScan("life.majiang.community.mapper")
+@EnableScheduling
+public class CommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityApplication.class, args);
+    }
+
+}
